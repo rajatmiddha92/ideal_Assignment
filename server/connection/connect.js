49b0df8b23fdba518957ea1ea9a51e1974476f68
Rajat <rajatmiddhaw92@gmail.com>
2023-04-04 17:41:05 +0530

    remote db added

--- server/connection/connect.js
+++ server/connection/connect.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
 
-mongoose.connect(`mongodb://localhost:27017/idealCareers`)
+mongoose.connect(`mongodb+srv://rajatmiddha92:idealcareers@cluster0.rjdeags.mongodb.net/?retryWrites=true&w=majority`)
 
 
